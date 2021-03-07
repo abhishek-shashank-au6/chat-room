fc358d15dcbe1dbe7bac4c91196414df4df8a2c5
abhishek-shashank-au6 <abhishek.shashank02@gmail.com>
2021-03-07 16:36:45 +0530

    Merge New FIx

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,27 +4,31 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-#Databse settings
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
 
 config :chat, Chat.Repo,
   # ssl: true,
-  host: "127.0.0.1",
-  port: 5432,
-  database: "chat_prod",
-  username: "postgres",
-  password: "abhishek",
-  pool_size: 10
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-#Web Endpoint settings
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") || "KzouT+ZDrRiN2koB0vfB20UVkGhlamUkV/li0ZCxIO/4uwuSg9l1uFlhfB0R/MtL"
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
 config :chat, ChatWeb.Endpoint,
-  server: true,
-  http: [port: 4000,  transport_options: [socket_opts: [:inet6]]],
-  url: [host: "139.59.38.199", port:80],
-  check_origin: [
-    "http://139.59.38.199"
-    "http://139.59.38.199:4000"
-  ]
-  secret_key_base: "qNUhygNa5vioTE0pQuChsCqTe5B9km4EXmXh6GRV/YH+tZ1uKmEHjc0L/n/zNz20"
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
 #
